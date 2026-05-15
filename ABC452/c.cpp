2c05451a81834f6afb1937648f40d82450ffd981
Pengfei Xu <pengfeixu@Pengfeis-MacBook-Air.local>
2026-05-15 09:45:51 +0800

    Update ABC452 C.cpp code (fix indentation)

--- ABC452/c.cpp
+++ ABC452/c.cpp
@@ -8,28 +8,27 @@ int  b[11];
 bool f[11][11][26];
 
 int main(){
-	int n; cin >> n;
-	for(int i=0;i<n;++i)
+    int n; cin >> n;
+    for(int i=0;i<n;++i)
 		cin >> a[i] >> b[i];
-	int m; cin >> m;
-	for(int i=1;i<=m;++i)
-		cin >> s[i];
-	for(int i=1;i<=m;++i){
-		int len = s[i].size();
-		for(int j=0;j<len;++j)
-			f[len][j+1][s[i][j]-'a'] = true;
-	}
-	for(int i=1;i<=m;++i){
-		int len = s[i].size();
-		if(len!=n){
-			cout << "No\n";
-			continue;
-		}
-		bool flag = true;
-		for(int j=0;j<n;++j){
+    int m; cin >> m;
+    for(int i=1;i<=m;++i)
+        cin >> s[i];
+    for(int i=1;i<=m;++i){
+        int len = s[i].size();
+        for(int j=0;j<len;++j)
+            f[len][j+1][s[i][j]-'a'] = true;
+    }
+    for(int i=1;i<=m;++i){
+        int len = s[i].size();
+        if(len!=n){
+            cout << "No\n";
+            continue;
+        }
+        bool flag = true;
+        for(int j=0;j<n;++j)
 			flag &= f[a[j]][b[j]][s[i][j]-'a'];
-		}
-		cout << (flag?"Yes\n":"No\n");
-	}
-	return 0;
+        cout << (flag?"Yes\n":"No\n");
+    }
+    return 0;
 }
